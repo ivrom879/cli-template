76e88b247b9dba72ee561355b0a17d69a223749a
ivrom879 <ivrom879@yandex.ru>
2020-07-27 14:13:09 +0300

    added user

---
+++ src/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Php\Package;
+
+/* use Illuminate\Support\Collection; */
+
+class User
+{
+    private $name;
+    private $children;
+
+    public function __construct(string $name, array $children = [])
+    {
+        $this->name = $name;
+        $this->children = collect($children);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+}
